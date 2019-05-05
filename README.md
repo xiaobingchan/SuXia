f38d84ec6b209b8f5954db1b52c11cf605bf267a
xiaobingchan <1755337994@qq.com>
2019-05-05 09:10:51 +0800

    加上一篇博客

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SuXia
 A docker source code rebuild framework
+
+Go leetcode解答：https://github.com/aQuaYi/LeetCode-in-Go
+
+Go 初学者之路：https://www.jianshu.com/p/5ee783bbdcac
---
+++ basic.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	var hp int = 100
+	hp = 200
+	var a = 200
+	hp = a * hp
+
+	//var b string
+	//var c []float32
+	//var d func() bool
+	/*var e struct{
+		x int
+	}*/
+	fmt.Println("hello world!")
+}
