9a3b13fc3316a6f3ccb98e50c05dda778cc19d42
xiaobingchan <1755337994@qq.com>
2019-05-26 23:43:24 +0800

    定义整形和字符串

---
+++ src/基本程序2_1.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main2() {
+	fmt.Printf("Hello, world\n")
+	fmt.Printf("你好，世界\n")
+	fmt.Printf("καλημ ́ρα κóσμ\n")
+	fmt.Printf("こんにちはせかい\n")
+}
---
+++ src/整型与浮点型声明2_2.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var int_a = 2
+	fmt.Println(int_a)//打印整形，用PrintLn
+	var float32_a = 2.349563487558
+	fmt.Println(float32_a) //打印浮点型
+}
